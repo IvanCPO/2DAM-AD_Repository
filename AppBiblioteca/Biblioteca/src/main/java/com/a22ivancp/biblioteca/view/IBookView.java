726e797629105d22023cbdc332c810974f053f4d
Ivan Cabaleiro Poceiro <a22ivancp@iessanclemente.net>
2024-01-23 10:25:13 +0100

    Preparado para hacer ejercicio Librería

---
+++ AppBiblioteca/Biblioteca/src/main/java/com/a22ivancp/biblioteca/view/IBookView.java
@@ -0,0 +1,22 @@
+package com.a22ivancp.biblioteca.view;
+
+public interface IBookView {
+
+    public void setBookTitle(String title);
+
+    public void setAuthor(String author);
+
+    public void setISBN(String isbn);
+
+    public void setYear(int year);
+
+    public void setAvailable(boolean available);
+
+    public void setCover(byte[] cover);
+
+    public void setID(int id);
+
+    public void setVisible(boolean visible);
+
+
+}
--- Biblioteca/src/main/java/org/a22ivancp/DAO.java
+++
@@ -1,8 +0,0 @@
-package org.a22ivancp;
-
-public interface DAO <T>{
-    void create (T object);
-    void delete (T object);
-    void update (T object);
-    T read (Long id);
-}
